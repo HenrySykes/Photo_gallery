e387ad8dca9093c5105436386a00408cdce0445b
HenrySykes <bazasykes@outlook.com>
2023-03-21 09:20:31 +0000

    repaired baguet box and creating own search filter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-const search = new Filter('search', 'data-caption');
 
-baguetteBox.run ('.gallery');
+baguetteBox.run('.gallery');
+
+let searchBox = document.querySelector('#search-box');
+let images = document.querySelectorAll('.input-container .image-container .photo-1 .photo-2 .photo-3 .photo-4 .photo-5 .photo-6 .photo-7 .photo-8 .photo-9 .photo-10 .photo-11 .photo-12');
+
+searchBox.oninput = () =>{
+    images.forEach(hide => hide.style.display = 'none');
+    let value = searchBox.value;
+    images.forEach(filter =>{
+        let title = filter.getAttribute('data-caption');
+        if(value == title){
+            filter.style.display = 'block';
+        }
+        if(searchBox.value = ''){
+            filter.style.display = 'block';
+        }
+    })
+}
