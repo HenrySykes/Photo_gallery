9dce319d6b9da4d2d847ab07712d03a5544ba814
HenrySykes <bazasykes@outlook.com>
2023-03-23 16:23:48 +0000

    fixed grid so that it is responsive without media queries and also starting search filter

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 
 baguetteBox.run('.gallery');
 
-let searchBox = document.querySelector('#search-box');
-let images = document.querySelectorAll('.input-container .image-container .photo-1 .photo-2 .photo-3 .photo-4 .photo-5 .photo-6 .photo-7 .photo-8 .photo-9 .photo-10 .photo-11 .photo-12');
 
-searchBox.oninput = () =>{
-    images.forEach(hide => hide.style.display = 'none');
-    let value = searchBox.value;
-    images.forEach(filter =>{
-        let title = filter.getAttribute('data-caption');
-        if(value == title){
-            filter.style.display = 'block';
-        }
-        if(searchBox.value = ''){
-            filter.style.display = 'block';
+// const anchors = document.querySelectorAll('a[data-caption]');
+
+// searchBox.addEventListener('keyup', e =>{
+//   console.log(e.target.value)
+//   console.log(anchors)
+// })
+
+const searchBox = document.getElementById('search-box');
+searchBox.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowerCase();
+    console.log(currentValue);
+    let search = document.querySelectorAll('class, data-caption');
+    search.forEach(photo => {
+        if (photo.textContent.toLowerCase().includes(currentValue)) {
+            photo.parentNode.parentNode.style.display = 'block';
+        } else {
+            photo.parentNode.parentNode.style.display = 'none';
         }
     })
-}
+});
